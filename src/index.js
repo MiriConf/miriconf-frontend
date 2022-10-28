c57d68bcc1768193c0c49f1b4c1009650a01ff8f
Newbtree45 <56883501+Newbtree45@users.noreply.github.com>
2022-10-28 19:06:45 -0400

    Update index.js
    Added code to display teams page in index js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const http = require('http');
+let fs = require('fs');
  
 const host = '0.0.0.0';
 const port = 8080;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('TESTING');
-});
- 
-server.listen(port, host, () => {
-   console.log('running at http://%s:%s',host,port );
+
+let handleRequest = (request, response) => {
+  response.writeHead(200, {
+      'Content-Type': 'text/html'
+  });
+  fs.readFile('./templates/teams.html', null, function (error, data) {
+      if (error) {
+          response.writeHead(404);
+          respone.write('Whoops! File not found!');
+      } else {
+          response.write(data);
+      }
+      response.end();
+  });
+};
+
+http.createServer(handleRequest).listen(port, host, () => {
+  console.log('running at http://%s:%s',host,port );
 });
